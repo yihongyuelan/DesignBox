f5c7e8d6dc72632484ff0ec3e3969fe024164276
yihongyuelan <yihongyuelan@gmail.com>
2016-06-02 09:05:17 +0800

    delete pages info

--- app/src/main/java/com/seven/demobox/designpatterns/patterns/factory/SampleFragment.java
+++ app/src/main/java/com/seven/demobox/designpatterns/patterns/factory/SampleFragment.java
@@ -11,30 +11,20 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 public class SampleFragment extends Fragment {
-    private String title;
-    private int page;
     public static SampleFragment newInstance(int page, String title) {
         final SampleFragment fragment = new SampleFragment();
-        Bundle bundle = new Bundle();
-        bundle.putInt("someInt", page);
-        bundle.putString("someTitle", title);
-        fragment.setArguments(bundle);
         return  fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        page = getArguments().getInt("someInt");
-        title = getArguments().getString("someTitle");
     }
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.factory_fragment, container, false);
-        TextView tvLabel = (TextView) view.findViewById(R.id.page_tv);
-        tvLabel.setText(page + " -- " + title);
         return view;
     }
 }
