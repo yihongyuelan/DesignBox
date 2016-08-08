08fce604bf678de63b6d450a927b811f8294a27b
yihongyuelan <yihongyuelan@gmail.com>
2016-08-08 09:46:18 +0800

    Add AutoLayout support and redesign the command layout.

--- app/src/main/java/com/seven/demobox/designpatterns/patterns/command/CommandFragment.java
+++ app/src/main/java/com/seven/demobox/designpatterns/patterns/command/CommandFragment.java
@@ -1,7 +1,13 @@
 package com.seven.demobox.designpatterns.patterns.command;
 
+import com.seven.demobox.R;
+
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 public class CommandFragment extends Fragment{
 
@@ -10,4 +16,9 @@ public class CommandFragment extends Fragment{
         super.onCreate(savedInstanceState);
     }
 
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.command_fragment, null);
+    }
 }
