4fd3964323d3e01333c31a3be35511c6c4ec29cf
yihongyuelan <yihongyuelan@gmail.com>
2017-08-31 09:41:38 +0800

    Add Compound Pattern moudle, part I

---
+++ app/src/main/java/com/seven/designbox/designpatterns/patterns/compound/view/PlayerDetailsViewMvcImpl.java
@@ -0,0 +1,44 @@
+/*
+* Copyright 2016 Seven_Tang <yihongyuelan@gmail.com>
+*
+* Licensed under the Apache License, Version 2.0 (the "License"); you may
+* not use this file except in compliance with the License. You may obtain
+* a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+package com.seven.designbox.designpatterns.patterns.compound.view;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class PlayerDetailsViewMvcImpl implements PlayerDetailsViewMvc {
+    private View mRootView;
+    private ShowDetailsViewListener mListener;
+    private boolean mMoreInfoSupported = true;
+    @Override
+    public View getRootView() {
+        return null;
+    }
+
+    @Override
+    public Bundle getViewState() {
+        return null;
+    }
+
+    @Override
+    public void moreInfoNotSupported() {
+
+    }
+
+    @Override
+    public void setListener(ShowDetailsViewListener listener) {
+
+    }
+}
